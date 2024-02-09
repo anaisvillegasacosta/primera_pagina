f5ff204197dd111b777e29378b8cc7489d0066b7
anaisvacosta <anaisvacosta@gmail.com>
2024-02-09 12:02:38 -0400

    clase 8

---
+++ script.js
@@ -0,0 +1,46 @@
+let variableUno = "Hola mundo Anais"
+let variableDos = 4
+let variableTres = true
+let variableCuatro = undefined
+let variableCinco = null
+
+let variableSeis = [
+    "Manzana",
+    "Pera",
+    "Caraota"
+]
+
+let usuario1 = {
+    name: "José José",
+    lastname: "Páez Páez",
+    age: 26,
+    email: "josejose@paez.go",
+    address: {
+        country: "Venezuela",
+        State: "Carabobo"
+    },
+    Chlidren: [
+        "Miguel",
+        "Camila",
+        "José",
+        "María, María"
+    ]
+
+}
+
+if (false + false) {
+    console.log("Se cumplió la condición")
+}
+
+else {
+    console.log("SI lo de arriba no se cumple")
+}
+
+let miFunción = (num1, num2) => {
+    return num1 + num2 
+}
+
+console.log
+    (
+        miFunción(7,8)
+)
